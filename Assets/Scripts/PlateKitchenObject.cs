c8a12c5da6b372f33f6c1ee813b8afd553c078bc
Hüseyin Çağatay DOĞRUYOL <61147184+HCD-dev@users.noreply.github.com>
2026-02-13 18:51:00 +0300

    Plate obje ekleme Görsel eklendi  Iconlarda eklendi

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;    
     private List<KitchenObjectSO> kitchenObjectSOList;
 
@@ -28,12 +37,19 @@ public class PlateKitchenObject : KitchenObject
         else
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
     }
 
 
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
 
 
- 
 }
